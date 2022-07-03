3081fe7ec36bc14aed29fe21806e002dd18296dd
LiaoChunping <2964556627@qq.com>
2022-07-03 22:47:29 +0800

    优化图片

--- inc/content.php
+++ inc/content.php
@@ -53,7 +53,7 @@
                                     <div class="align-self-center post-cc">
                                         <a rel="license" href="http://creativecommons.org/licenses/by/4.0/deed.zh"><img
                                                     alt="知识共享许可协议" style="border-width:0"
-                                                    src="https://i.creativecommons.org/l/by/4.0/80x15.png"/></a><br/><br/>
+                                                    src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAAAPCAMAAABEF7i9AAAABGdBTUEAANbY1E9YMgAAAJZQTFRF////7u7u3d3dys7KzMzMyMzIxsrGxcbFur+6u7u7s7iyq7GqqqqqmZmZlJmTj5CPiIiIh4eHhoaGgICAfYJ9d3d3cnZxZ2tnZmZmW15bVVVVS0xLREREQ0NDQkJCQUJBOz07OTs5MzMzMTMxLjAuJygnJCUjIiIiISEhICAgGRkZERERDxAPDg4ODQ4NDQ0NDQ0MAAAADbeuvgAAAMNJREFUeNqtk9sSgiAQQJekslaNLpgZ3exiZWr7/z/XEI6N00spO/DCMocDywJZDiCwGhqIOpYkqiWVB4jOo7WfAQa5qA9RZ0R33hG4v36sOa0Q++tuwEIAT0kxRSkHdUR0Ju88gN5k5j/ABY0gjUHKjPkeyALRHZq8GfCvYUic6arEib6zzBHHg9rwd78vQxVlTPogy6ZhCyCWAryMIqYo6cHm1HjDVsDDzXKVg+e0Bm4vFv4hhjSreLt7106x3cuW4wXCCZ5As6hO5AAAAABJRU5ErkJggg=="/></a><br/><br/>
                                         本作品采用<a rel="license" href="http://creativecommons.org/licenses/by/4.0/deed.zh">知识共享署名
                                             4.0 国际许可协议</a>进行许可
                                     </div>
