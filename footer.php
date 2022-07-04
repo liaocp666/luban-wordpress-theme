361e6f061c5a4275f3b5a50399ac27741129df08
LiaoChunping <2964556627@qq.com>
2022-07-04 12:21:08 +0800

    百度代码统计

--- footer.php
+++ footer.php
@@ -66,5 +66,14 @@
     </div>
 </footer>
 <?php wp_footer(); ?>
+<script>
+    var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?56a449dda858f975e981265933d4634e";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+</script>
 </body>
 </html>
